490d128143c2bf40df08cfbcd78c790121d2dd5e
Charlie Lashmar <charlie@lashmar.com>
2025-03-06 14:27:09 +0000

    Create Note model

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,6 @@ from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all
     serializer_class = UserSerializer
